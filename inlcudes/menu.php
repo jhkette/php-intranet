8e005503a8e0f48190e335b20077c745fd1b8d2f
khenryjoe <joseph.ketterer@gmail.com>
2018-12-29 22:18:15 +0000

    add index page amend menu

--- inlcudes/menu.php
+++ inlcudes/menu.php
@@ -2,17 +2,19 @@
 
 if(isset($_SESSION['admin'])) {
     $menu = [
-    'logout.php'  => 'Logout',
+    'index.php'  => 'Home',  
     'DTresults.php' => 'DT results',
     'P1results.php' => 'P1 results',
     'PfPresults.php' => 'PfP results',
     'add-user.php' => 'Add User',
+    'logout.php'  => 'Logout',
     ];
 }
 
 
 else{
     $menu = [
+    'index.php'  => 'Home',
     'admin-login.php'  => 'Admin Login',
     'login.php'  => 'Login',
     'DTresults.php' => 'DT results',
