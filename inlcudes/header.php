a4a4756f98953b60d7f5530a00ca907022f5c4f8
khenryjoe <joseph.ketterer@gmail.com>
2019-01-06 02:28:58 +0000

    dsf

--- inlcudes/header.php
+++ inlcudes/header.php
@@ -1,6 +1,3 @@
-
-
-
 <?php
 echo "<header class='header'>".PHP_EOL.
          "<h1>DCS Intranet</h1>".PHP_EOL.
--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@
 $_SESSION = array(); #unset session - removes all stored data from array
 if (ini_get("session.use_cookies")) { #if using cookies
     /* Delete session cookie */
-    $yesterday = time() - (24 * 60 * 60);
+    $yesterday = time() - (48 * 60 * 60);
     $params = session_get_cookie_params();
     setcookie(session_name(), '', $yesterday,
         $params["path"], $params["domain"],
