79fe58355f815fce31fb3f1fe216456c22732809
khenryjoe <joseph.ketterer@gmail.com>
2019-01-14 17:42:15 +0000

    caching

--- inlcudes/init.php
+++ inlcudes/init.php
@@ -1,9 +1,11 @@
 <?php session_start();
+/* nb I'm refreshing the page each time the page loads. I was having problems with cached pages displaying old
+session data on the BBK titan server */
 header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate( 'D, d M Y H:i:s') . ' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache'); 
+header('Pragma: no-cache');
 require_once('functions.php');
 include('inlcudes/menu.php');
 
