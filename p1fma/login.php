0d4dd706c242515a2308a989cf27240ab23c3e40
khenryjoe <joseph.ketterer@gmail.com>
2019-01-17 10:52:15 +0000

    remove extra typo  'if (isset($_POST['submit'])) { 'on login.php

--- p1fma/login.php
+++ p1fma/login.php
@@ -64,22 +64,20 @@ else{
                           or redirects the user to welcome.php if no errors were detected */
                           if (isset($_POST['submit'])) {
                               $formSubmmited = true;
-                              /*i'm counting the size of the errors arry for validation
+                              /*i'm counting the size of the errors array for validation
                               if errors > 0 the form is invalid */
-                              if (isset($_POST['submit'])) {
-                                  switch (true) {
-                                       case (isset( $_SESSION['admin'])) :
-                                       echo '<p class="message"> Please logout first </p>'; # checking they are not logged in as an admin
-                                       break;
-                                       case (count($cleanData) < 2) :
-                                       echo displayErrors($errors); # clean data is less than 2 so display errors
-                                       break;
-                                       case (count($cleanData) == 2): # clean data == 2 - no errors so redirect to index
-                                       header('Location: index.php');
+                               switch (true) {
+                                   case (isset( $_SESSION['admin'])) :
+                                    echo '<p class="message"> Please logout first </p>'; # checking they are not logged in as an admin
+                                    break;
+                                    case (count($cleanData) < 2) :
+                                    echo displayErrors($errors); # clean data is less than 2 so display errors
+                                    break;
+                                    case (count($cleanData) == 2): # clean data == 2 - no errors so redirect to index
+                                    header('Location: index.php');
+                                }
+                            }
 
-                                   }
-                              }
-                          }
                          ?>
                          <p class ="message">
                              <!-- This shows if user trys to view results without logging in -->
